aa7406f8005e85e355f0ebb137a340f046cd57c3
vietduc1441 <vietduc1441@yahoo.com>
2016-03-27 12:45:50 +0200

    run from command line

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 /// reference <path="./typings/tsd.d.ts">;
 import EditingUtils=require("./editingUtils");
-import project=require("./config");
 var Editing=new EditingUtils();
-var moduleNames=process.argv.slice(2)
-export function autolayoutThenCommit(){
+export function autolayoutThenCommit(project,moduleNames){
     Editing.load(project)
     .then(()=> Editing.autolayouProject(moduleNames))
     .then(()=> Editing.commitToServer())
